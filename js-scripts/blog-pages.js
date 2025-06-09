93e18c843e5e0cec151b94fc8bba68d9d0381715
Elliott <54526461+ItsJayhad@users.noreply.github.com>
2025-06-09 11:17:02 -0700

    Add new blog posts and enhance blog structure with CSS updates

---
+++ js-scripts/blog-pages.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        document.getElementById('fade-overlay')?.classList.add('fade-out');
+    }, 200); // slight delay for effect
+});
